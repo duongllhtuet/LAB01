5dedf56e2abcd092518340aa4a16daaf749fd6ca
Duong <leduong.uet.ai@gmail.com>
2023-09-12 11:46:25 +0700

    Lab01

---
+++ HomeWork1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    if (n & 1) {
+        cout << "So Le";
+    } else {
+        cout << "So chan";
+    }
+
+    return 0;
+}
